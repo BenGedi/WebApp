32d57740d374287f7f8daf81eaca84fe0a6813c8
Ben Gedi <bengedi@gmail.com>
2014-11-03 15:28:58 +0200

    fix margins

--- js/main.js
+++ js/main.js
@@ -1,1 +1,12 @@
-
+document.getElementById('btn-settings').addEventListener('click',function(e){
+	var target = e ? e.target : window.event.srcElement;
+	var hasActive = target.classList.contains('active');
+	var setting = document.getElementById('settings');
+	if(hasActive){
+		target.classList.remove('active');
+		setting.classList.add('hidden');
+	}else {
+		target.classList.add('active');
+		setting.classList.remove('hidden');
+	}
+});
