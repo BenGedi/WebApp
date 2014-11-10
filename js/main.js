e35f078d331558abd9aba133cf4de5d36577c4d7
Ben Gedi <bengedi@gmail.com>
2014-11-10 02:23:58 +0200

    update using UTILS library add hidden class to: setting select, settings target link button,tab content

--- js/main.js
+++ js/main.js
@@ -1,5 +1,11 @@
 window.onload = (function() {
-	var tabActive = function(tabs){
+    var TabsCollection = document.querySelectorAll('.tabs a'),
+        TabsContentCollection = document.querySelectorAll('.tab'),
+        notification = UTILS.qs('.notifications');
+
+        notification.classList.add('hidden');
+
+    var tabActive = function(tabs){
                     for(var i = 0; i< tabs.length ;i++){
                         if (tabs[i].classList.contains('tab-active')){
                             return tabs[i];
@@ -14,80 +20,99 @@ window.onload = (function() {
             else{
                 return tabsContent[i];
             }
-        };
+        }
     };
     var getAtagByHash = function(hash){
-        for (var i = 0; i < myLinksCollection.length; i++) {
-            if (myLinksCollection[i].hash === ("#"+hash)){
-                return myLinksCollection[i];
+        for (var i = 0; i < TabsCollection.length; i++) {
+            if (TabsCollection[i].hash === ("#"+hash)){
+                return TabsCollection[i];
             }
-        };
+        }
     };
+    var settingsBtnCheck = function(e){
+        var target = e ? e.target : window.event.srcElement;
+        var hasActive = target.classList.contains('active');
+        var setting = document.getElementById('settings');
+        if(hasActive){
+            target.classList.remove('active');
+            setting.classList.add('hidden');
+        }
+        else {
+            target.classList.add('active');
+            setting.classList.remove('hidden');
+        }
+    };
+
+
 
     /*
     * checkHash function is adding and removing classes
     * for activate "a" tabs and relevant divs
     *
     * There is 2 ways that checkHash is running:
-    * 1. by the addEventListener.
-    * 2. by refresh url + #id
+    * 1. addEventListener('click' , checkHash)
+    * 2. addEventListener('hashchange' , checkHash)
     */
     var checkHash = function(e){
-        // variable "that" checking if "e" has an event (from addEventListener)
-        // yes: that gets the event currentTarget
-        // no: that get the window.location
-        var that = (e !== undefined) ? e.currentTarget : location;
+        if (e.newURL || e.path.length !== 0){
+           e.preventDefault();
+           // variable "that" checking if "e" is a window event
+           // yes: that gets window new url.
+           // no: that gets the targeted a tab element.
+           var that = e.newURL ? e.newURL : e.currentTarget.href;
 
-        // checking if user press on an active tab or refreshed the same url + #id
-        if (currentTab === that || that.href === currentTab.href){
-            return false;
-        }
+           // checking if user press on an active tab or refreshed the same url + #id
+           if (currentTab === that || that === currentTab.href){
+               return false;
+           }
 
-        // remove class active form current tab
-        currentTab.classList.remove('tab-active');
-        // add class hidden to current tab content
-        currentTabContent.classList.add('hidden');
+           // remove class active form current tab
+           currentTab.classList.remove('tab-active');
+           // add class hidden to current tab content
+           currentTabContent.classList.add('hidden');
 
-
-        if (that.href.indexOf("#") !== -1) {
-            var clickedHREF = that.href,
+           if (that.indexOf("#") !== -1) {
+                var clickedHREF = that,
                 clickedView = clickedHREF.split("#"),
                 showTabContent = document.getElementById(clickedView[1]),
                 // aTag is the tab target
-                aTag = e !== undefined ? e.target :getAtagByHash(clickedView[1]);
-
-            // activate target tab
-            aTag.classList.add('tab-active');
-            showTabContent.classList.remove('hidden');
-            currentTab = aTag; // initialize current tab
-            currentTabContent = showTabContent; // initialize current tab
-            location.hash = clickedView[1];
+                aTag = getAtagByHash(clickedView[1]);
+                // activate target tab
+                aTag.classList.add('tab-active');
+                showTabContent.classList.remove('hidden');
+                currentTab = aTag; // initialize current tab
+                currentTabContent = showTabContent; // initialize current tab
+                location.hash = clickedView[1];
+            }
+        }
+        else{
+            return false;
         }
     };
-    var myLinksCollection = document.querySelectorAll('.tabs a');
-    var myTabsContentCollection = document.querySelectorAll('.tab');
-    var currentTabContent = tabActiveContent(myTabsContentCollection);
-    var currentTab = tabActive(myLinksCollection);
-    var currentHash = location.hash;
-    if (currentHash !=="") {
-        checkHash();
-    }
-    for (i=0;i<myLinksCollection.length;i++) {
-        myLinksCollection[i].addEventListener('click',checkHash);
+    var dispatchEvt = function(){
+                window.dispatchEvent(new Event("hashchange"));
+            };
+
+    var currentTabContent = tabActiveContent(TabsContentCollection),
+        currentTab = tabActive(TabsCollection),
+        currentHash = location.hash;
+    UTILS.addEvent(document.getElementById('btn-settings'),'click',settingsBtnCheck);
+    UTILS.addEvent(window,'hashchange',checkHash);
+    // window.addEventListener('load' ,function(){
+    //     window.dispatchEvent(new Event("hashchange"));
+    // });
+    UTILS.addEvent(window,'load',dispatchEvt);
+    UTILS.ajax('../data/notification.txt', {
+        done: function(response) {
+            notification.classList.remove('hidden');
+            notification.innerHTML = response;
+        },
+        fail: function(err){
+            console.log(err);
+        }
+    });
+    for (i=0;i<TabsCollection.length;i++) {
+        UTILS.addEvent(TabsCollection[i],'click',checkHash);
     }
 })();
 
-
-document.getElementById('btn-settings').addEventListener('click',function(e){
-	var target = e ? e.target : window.event.srcElement;
-	var hasActive = target.classList.contains('active');
-	var setting = document.getElementById('settings');
-	if(hasActive){
-		target.classList.remove('active');
-		setting.classList.add('hidden');
-	}
-	else {
-		target.classList.add('active');
-		setting.classList.remove('hidden');
-	}
-});
